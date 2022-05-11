585e3abdebaa13d6d20b8b864752a73f5b2a549e
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-11 11:48:30 +0200

    40-reactJs Codelab04 filtering complete

--- src/components/ProfileGallery.jsx
+++ src/components/ProfileGallery.jsx
@@ -7,22 +7,49 @@ export default function ProfileGallery() {
         getPets().then((result) => setPets(result.data));
     }, pets)
 
-    return (
-        <div id="gallery" className="col-md-9">
-            <div className="gallery">
-                {pets.map(pet => (
-                        <div className="gallery-pet fader" key={pet.id}>
-                            <img className="profile-picture" src={"https://pettinder.herokuapp.com/" + pet.image}
-                                 alt="pet-picture"/>
-                            <div className="overlay">
-                                <div className="overlay-text">{pet.name}
+    let filteredPets;
+    const [filterText, setFilterText] = useState([]);
+    if (filterText === '' || filterText === undefined) {
+        filteredPets = pets;
+    } else {
+        filteredPets = pets.filter(pet => pet.name.toLowerCase().includes(filterText.toString().toLowerCase()));
+    }
+
+    return (<>
+            <div id="gallery" className="col-md-9">
+                <div className="gallery">
+                    {filteredPets.map(pet => (
+                            <div className="gallery-pet fader" key={pet.id}>
+                                <img className="profile-picture" src={"https://pettinder.herokuapp.com/" + pet.image}
+                                     alt="pet-picture"/>
+                                <div className="overlay">
+                                    <div className="overlay-text">{pet.name}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                )}
+                        )
+                    )}
+                </div>
             </div>
-        </div>
+            <div className="col-md-3">
+                <div className="gallery-detail">
+                    <div>
+                        <section className="tiny-dialog">
+                            <h3>Find your pet</h3>
+                            <div className="form-group">
+                                <input
+                                    className="form-control"
+                                    id="name"
+                                    name="name"
+                                    value={filterText}
+                                    onChange={(event) => setFilterText(event.target.value)}
+                                    placeholder="Name"
+                                />
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </>
     );
-
 }
