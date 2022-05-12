9ddb8dd49f9ae2009c9e380809d21756cad694cf
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-12 15:37:15 +0200

    Codelab07 Redirection with button and increment popularity ... but with my own implementation

--- src/api/PetService.js
+++ src/api/PetService.js
@@ -1,13 +1,17 @@
 import apiClient from './Client'
 
-export function getPets(){
-    return apiClient.get('pets')
+export function getPets() {
+    return apiClient.get('pets');
 }
 
-export function addPets(pet){
-    return apiClient.post('pets', pet)
+export function addPets(pet) {
+    return apiClient.post('pets', pet);
 }
 
-export function deletePet(petId){
-    return apiClient.delete('pets/'+ petId)
+export function deletePet(petId) {
+    return apiClient.delete('pets/' + petId);
+}
+
+export function addPetPopularity(petName) {
+    return apiClient.get('pets/' + petName + '/incrementPopularity');
 }
--- src/components/SetupDate.jsx
+++ src/components/SetupDate.jsx
@@ -1,37 +1,50 @@
 import React from "react";
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {addPetPopularity} from "../api/PetService";
 
-export default function SetupDate(){
+export default function SetupDate() {
 
     const location = useLocation();
     const {selectedPet} = location.state.selectedPet;
 
+    function incrementPopularity() {
+        addPetPopularity(selectedPet.name).then(result => console.log('Popularity increase :' + result));
+    }
+
     return (<>
-        <div className="container modalPopup">
-            <div className="row">
-                <div className="col-md-8 col-md-offset-2">
-                    <div className="big-dialog">
-                        <div className="clearfix">
-                            <div className="pull-left dialog-picture">
-                                <img
-                                    src={"https://pettinder.herokuapp.com/" + selectedPet.image}
-                                    className="profile-picture"
-                                    alt={"Picture of " + selectedPet.name}
-                                />
+            <div className="container modalPopup">
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2">
+                        <div className="big-dialog">
+                            <div className="clearfix">
+                                <div className="pull-left dialog-picture">
+                                    <img
+                                        src={"https://pettinder.herokuapp.com/" + selectedPet.image}
+                                        className="profile-picture"
+                                        alt={"Picture of " + selectedPet.name}
+                                    />
+                                </div>
+                                <h3>{selectedPet.name}</h3>
+                                <p>{selectedPet.profileText}</p>
                             </div>
-                            <h3>{selectedPet.name}</h3>
-                            <p>{selectedPet.profileText}</p>
-                        </div>
-                        <br />
-                        <div className="clearfix">
-                            <form onSubmit="play-function">
-                                <button className="btn btn-primary pull-right" type="button">Cancel</button>
-                                <button className="btn btn-primary pull-right" type="submit">Let us Play</button>
-                            </form>
+                            <br/>
+                            <div className="clearfix">
+                                {/*<form onSubmit="play-function">*/}
+                                    <Link to="/">
+                                        <button className="btn btn-primary pull-right" type="button">Cancel</button>
+                                    </Link>
+                                    <Link to="/">
+                                        <button className="btn btn-primary pull-right"
+                                                onClick={() => incrementPopularity()}
+                                                type="submit">Let us Play
+                                        </button>
+                                    </Link>
+                                {/*</form>*/}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>);
+        </>
+    );
 }
